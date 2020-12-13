b2fec0ac2117095857fe51563628c4d34c17c7ac
Matthew Biesen <mbiesen@clm-mbiesen.local>
2020-12-13 03:51:46 -0600

    refactor louisiana

--- state_scripts/louisiana.py
+++ state_scripts/louisiana.py
@@ -8,9 +8,7 @@ PRIMARY_REPRESENTATIVES_URL = 'https://house.louisiana.gov/H_Reps/H_Reps_Email'
 PRIMARY_SENATORS_URL = 'http://senate.la.gov/Senators/Offices.asp'
 
 rep_sh = state_helper(PRIMARY_REPRESENTATIVES_URL)
-rep_sh.prepare_soup()
 sen_sh = state_helper(PRIMARY_SENATORS_URL)
-sen_sh.prepare_soup()
 
 rep_texts = rep_sh.bs4_helper.get_text_for_span_tags()
 for text in rep_texts:

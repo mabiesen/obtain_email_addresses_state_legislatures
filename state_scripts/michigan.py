d1934a4741653d4aa7d5a42c5d9e60a1ce977351
Matthew Biesen <mbiesen@clm-mbiesen.local>
2020-12-13 03:55:31 -0600

    refactor michigan

--- state_scripts/michigan.py
+++ state_scripts/michigan.py
@@ -8,15 +8,12 @@ PRIMARY_SENATORS_URL = 'https://senate.michigan.gov/Senatoremaillst.html'
 PRIMARY_REPRESENTATIVES_URL = 'https://www.house.mi.gov/MHRPublic/frmRepListMilenia.aspx?all=true'
 
 sen_sh = state_helper(PRIMARY_SENATORS_URL)
-sen_sh.prepare_soup()
-sen_links = sen_sh.bs4_helper.get_hrefs_for_a_tag()
-for link in  sen_links:
-  if 'mailto:' in link:
-    print(link.replace('mailto:',''))
-
 rep_sh = state_helper(PRIMARY_REPRESENTATIVES_URL)
-rep_sh.prepare_soup()
-rep_links = rep_sh.bs4_helper.get_hrefs_for_a_tag()
-for link in  rep_links:
-  if 'mailto:' in link:
-    print(link.replace('mailto:',''))
+
+rep_addrs = rep_sh.get_mailto_addresses()
+for addr in rep_addrs:
+  print(addr)
+
+sen_addrs = sen_sh.get_mailto_addresses()
+for addr in sen_addrs:
+  print(addr)

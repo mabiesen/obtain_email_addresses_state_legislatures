bf532bb9f9197d7baf4bc64cd606fd9b15d2869e
Matthew Biesen <mbiesen@clm-mbiesen.local>
2020-12-13 00:59:50 -0600

    fail to add mississippi

---
+++ state_scripts/mississippi.py
@@ -0,0 +1,17 @@
+import os
+import sys
+sys.path.append(os.getcwd() + '/..')
+
+from state_helper import state_helper
+
+PRIMARY_REPRESENTATIVES_URL = 'http://www.legislature.ms.gov/legislators/representatives/'
+PRIMARY_SENATORS_URL = 'http://www.legislature.ms.gov/legislators/senators/'
+
+# pretty sure the rep links are loaded with javascript.....
+# cant find them
+
+#sen_sh = state_helper(PRIMARY_REPRESENTATIVES_URL)
+#sen_sh.prepare_soup()
+#links = sen_sh.bs4_helper.get_hrefs_for_a_tag()
+#for link in links:
+#  print(link)

daee6b0813d3b7c05a21c4c60f69a95ec0e3b22d
Matthew Biesen <mbiesen@clm-mbiesen.local>
2020-12-13 03:54:13 -0600

    refactor kansas

--- state_scripts/kansas.py
+++ state_scripts/kansas.py
@@ -8,9 +8,7 @@ PRIMARY_SENATORS_URL = 'http://www.kslegislature.org/li/b2019_20/chamber/senate/
 PRIMARY_REPRESENTATIVES_URL = 'http://www.kslegislature.org/li/b2019_20/chamber/house/roster/'
 
 sen_sh = state_helper(PRIMARY_SENATORS_URL)
-sen_sh.prepare_soup()
 rep_sh = state_helper(PRIMARY_REPRESENTATIVES_URL)
-rep_sh.prepare_soup()
 
 rep_addrs = rep_sh.get_mailto_addresses()
 for addr in rep_addrs:

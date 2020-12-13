a584d3a6d6df3d4a5766cca78297b3a4fe7d08b8
Matthew Biesen <mbiesen@clm-mbiesen.local>
2020-12-13 00:18:21 -0600

    add michigan

---
+++ state_scripts/michigan.py
@@ -0,0 +1,22 @@
+import os
+import sys
+sys.path.append(os.getcwd() + '/..')
+
+from state_helper import state_helper
+
+PRIMARY_SENATORS_URL = 'https://senate.michigan.gov/Senatoremaillst.html'
+PRIMARY_REPRESENTATIVES_URL = 'https://www.house.mi.gov/MHRPublic/frmRepListMilenia.aspx?all=true'
+
+sen_sh = state_helper(PRIMARY_SENATORS_URL)
+sen_sh.prepare_soup()
+sen_links = sen_sh.bs4_helper.get_hrefs_for_a_tag()
+for link in  sen_links:
+  if 'mailto:' in link:
+    print(link.replace('mailto:',''))
+
+rep_sh = state_helper(PRIMARY_REPRESENTATIVES_URL)
+rep_sh.prepare_soup()
+rep_links = rep_sh.bs4_helper.get_hrefs_for_a_tag()
+for link in  rep_links:
+  if 'mailto:' in link:
+    print(link.replace('mailto:',''))

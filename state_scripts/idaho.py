acc2830f53982f043b7ce01f9841ddef19417c16
Matthew Biesen <mbiesen@clm-mbiesen.local>
2020-12-13 03:43:15 -0600

    refactor idaho

--- state_scripts/idaho.py
+++ state_scripts/idaho.py
@@ -8,16 +8,12 @@ PRIMARY_SENATORS_URL = 'https://legislature.idaho.gov/senate/membership/'
 PRIMARY_REPRESENTATIVES_URL = 'https://legislature.idaho.gov/house/membership/'
 
 rep_sh = state_helper(PRIMARY_REPRESENTATIVES_URL)
-rep_sh.prepare_soup()
 sen_sh = state_helper(PRIMARY_SENATORS_URL)
-sen_sh.prepare_soup()
 
-rep_links = rep_sh.bs4_helper.get_hrefs_for_a_tag()
-for link in rep_links:
-  if 'mailto:' in link:
-    print(link.replace('mailto:',''))
+rep_addrs = rep_sh.get_mailto_addresses()
+for addr in rep_addrs:
+  print(addr)
 
-sen_links = sen_sh.bs4_helper.get_hrefs_for_a_tag()
-for link in sen_links:
-  if 'mailto:' in link:
-    print(link.replace('mailto:',''))
+sen_addrs = sen_sh.get_mailto_addresses()
+for addr in sen_addrs:
+  print(addr)

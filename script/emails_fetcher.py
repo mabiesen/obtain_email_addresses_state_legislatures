98222614e3b4ce6a5fa5790688491fdf3aa90560
Matthew Biesen <mbiesen@clm-mbiesen.local>
2020-12-13 22:02:25 -0600

    changes

--- script/emails_fetcher.py
+++ script/emails_fetcher.py
@@ -25,6 +25,29 @@ from state_scripts import mississippi
 from state_scripts import missouri
 from state_scripts import montana
 from state_scripts import nebraska
+from state_scripts import nevada
+from state_scripts import new_hampshire
+from state_scripts import new_jersey
+from state_scripts import new_mexico
+from state_scripts import new_york
+from state_scripts import north_carolina
+from state_scripts import north_dakota
+from state_scripts import ohio
+from state_scripts import oklahoma
+from state_scripts import oregon
+from state_scripts import pennsylvania
+from state_scripts import rhode_island
+from state_scripts import south_carolina
+from state_scripts import south_dakota
+from state_scripts import tennessee
+from state_scripts import texas
+from state_scripts import utah
+from state_scripts import vermont
+from state_scripts import virginia
+from state_scripts import washington
+from state_scripts import west_virginia
+from state_scripts import wisconsin
+from state_scripts import wyoming
 
 import sys
 

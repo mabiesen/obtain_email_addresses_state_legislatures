7b963d7ea3f83da71441f6250fe4e7d153ccc14a
Matthew Biesen <mbiesen@clm-mbiesen.local>
2020-12-14 22:30:35 -0600

    add nevada but doesnt work yet

--- script/state_scripts/nevada.py
+++ script/state_scripts/nevada.py
@@ -1,9 +1,23 @@
 from state_scripts.lib.state_helper import state_helper
 
 def run():
-  pass
+  senators_url = 'https://www.leg.state.nv.us/App/Legislator/A/Senate/'
+  representatives_url = 'https://www.leg.state.nv.us/App/Legislator/A/Assembly/'
 
-# Im not finding anything for state representatives!!!
+  # unfortunately these sites seem to use javascript, will have to wait
+
+  sen_sh = state_helper(senators_url)
+  rep_sh = state_helper(representatives_url)
+
+  rep_addrs = rep_sh.get_mailto_addresses()
+  for addr in rep_addrs:
+    print(addr)
+
+  sen_addrs = sen_sh.get_mailto_addresses()
+  for addr in sen_addrs:
+    print(addr)
+
+  return (rep_addrs + sen_addrs)
 
 if __name__ == "__main__":
   run()

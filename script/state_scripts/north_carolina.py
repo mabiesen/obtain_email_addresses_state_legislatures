171c21de1851115eced81a596e439606e537a2e0
Matthew Biesen <mbiesen@clm-mbiesen.local>
2020-12-16 01:52:41 -0600

    get half of north carolina

--- script/state_scripts/north_carolina.py
+++ script/state_scripts/north_carolina.py
@@ -1,9 +1,16 @@
 from state_scripts.lib.state_helper import state_helper
 
 def run():
-  pass
+  senators_url = 'https://www.ncleg.gov/Members/MemberInfoReport/S'
 
-# Im not finding anything for state representatives!!!
+  sen_sh = state_helper(senators_url, True)
+  sen_addrs = sen_sh.get_mailto_addresses()
+  for addr in sen_addrs:
+    print(addr)
+
+  # the best solution is a messy one
+  # house addrs will require a change of tactic
+  # we need to simulate button clicking on the site and download to a designated folder
 
 if __name__ == "__main__":
   run()

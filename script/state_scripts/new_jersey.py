bfd93aff0d77e54625e775ec4a929fdbcc5cd4ca
Matthew Biesen <mbiesen@clm-mbiesen.local>
2020-12-15 22:59:21 -0600

    fail to add new jersey

--- script/state_scripts/new_jersey.py
+++ script/state_scripts/new_jersey.py
@@ -1,6 +1,8 @@
 from state_scripts.lib.state_helper import state_helper
 
 def run():
+  # primary_url =  'https://www.njleg.state.nj.us/members/roster.asp'
+  # ^^ but we cant get email addresses, they dont exist, governed by server
   pass
 
 # Im not finding anything for state representatives!!!
